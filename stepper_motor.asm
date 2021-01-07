4ea16318b64a3ec567fc9c7fb3f92536120bd33f
hindemad <76590052+hindemad@users.noreply.github.com>
2021-01-07 14:22:57 +0200

    Update stepper_motor.asm

--- stepper_motor.asm
+++ stepper_motor.asm
@@ -1,2 +1,17 @@
 mov A, 20
 mov B, 33
+
+
+COLUMN0:
+        SETB P1.6		// Make the last column as output pin
+        CLR P1.4		// Make column 1 as output
+		MOV R0,#0D		// Store niohn number
+		MOV R5,#1D		// Llojmn with the next column number
+	    SJMP ROWS 		// Scan the key
+
+COLUMN2:
+		SETB P1.5
+        CLR P1.6
+		MOV R0,#2D
+		MOV R5,#0D
+	    SJMP ROWS 	
