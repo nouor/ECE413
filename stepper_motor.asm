46578f8f43f450d7683d50aa1241fcf624fda651
hindemad <76590052+hindemad@users.noreply.github.com>
2021-01-07 22:42:35 +0200

    Update stepper_motor.asm

--- stepper_motor.asm
+++ stepper_motor.asm
@@ -5,7 +5,7 @@ mov B, 33
 
 READ_PORT0: //READP0
 	JB P0.0,HALF_STEP //IF BIT =1 GO HALF STEP
-	/*******************************************************************************************
+/*******************************************************************************************
 *					SWITCHES
 ********************************************************************************************/
 
@@ -30,24 +30,54 @@ INTERFACING_KEYPAD:
 /* each time only one of the column pins will be output
    and the rest will be input pins include the row pins */
    
-MOV P1,#11111111B // loads P1 with all 1's
+MOV P1,#11111111B               // loads P1 with all 1's
 COLUMN0:
         SETB P1.6		// Make the last column as input pin
         CLR P1.4		// Make column 1 as output
-		MOV R0,#0D		// Store column number
-		MOV R5,#1D		// Load R5 with the next column number
-	    SJMP ROWS 		// Scan the rows to get the key
+	MOV R0,#0D		// Store column number
+	MOV R5,#1D		// Load R5 with the next column number
+	SJMP ROWS 		// Scan the rows to get the key
 
 COLUMN1:
-		SETB P1.4
+	SETB P1.4
         CLR P1.5 
-		MOV R0,#1D
-		MOV R5,#2D
-	    SJMP ROWS 
+	MOV R0,#1D
+	MOV R5,#2D
+	SJMP ROWS 
 		
 COLUMN2:
-		SETB P1.5
+	SETB P1.5
         CLR P1.6
-		MOV R0,#2D
-		MOV R5,#0D
-	    SJMP ROWS 		
+	MOV R0,#2D
+	MOV R5,#0D
+	SJMP ROWS 		
+ROWS:
+	 ROW0:
+		 JB P1.0,ROW1		// Checks whether ROW 0 is LOW (If key is pressed) and if not(P1.0 is 1) jump to ROW 1
+		 MOV R2,#0D			// Store ROW number
+		 SJMP CAPTURE			// Jump to CAPTURE to get the key value
+
+	 ROW1:
+		 JB P1.1,ROW2
+		 MOV R2,#1D
+		 SJMP CAPTURE
+
+	 ROW2:
+		 JB P1.2,ROW3
+		 MOV R2,#2D
+		 SJMP CAPTURE
+
+	 ROW3:
+		 JB P1.3,NEXT		// If ROW 3 is HIGH (If key is not pressed) Jumb to NEXT to get the next column
+		 MOV R2,#3D
+		 SJMP CAPTURE
+	 
+NEXT:		// Continuing to the next column (last column number is stored in R5)
+	 CLM0:
+		 CJNE R5,#0D,CLM1		// Jumb to Column 0 if the last column was column 2
+		 SJMP COLUMN0
+     CLM1:
+		 CJNE R5,#1D,CLM2
+		 SJMP COLUMN1
+	 CLM2:
+		 SJMP COLUMN2
