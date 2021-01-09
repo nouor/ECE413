c14ca6014e83cf1d0ac9f9b67b40929cc72f16cb
Maria-Kamel <76961013+Maria-Kamel@users.noreply.github.com>
2021-01-08 16:15:05 -0800

    Update stepper_motor.asm

--- stepper_motor.asm
+++ stepper_motor.asm
@@ -146,5 +146,6 @@ That means X = 50000
 65536 – X =  65536 – 50000 = 15536.
 64536 is considered in decimal and converting it t0 hexadecimal gives 3CB0
 That means THTL = 3C
+Therefore TH=FC and TL=B0
 
 */
